510c4125301eb902fc72fb77187b8c269c5b766c
iamMagma <98006497+iamMagma@users.noreply.github.com>
2022-02-02 07:40:31 +0530

    Update updater.js

--- plugins/updater.js
+++ plugins/updater.js
@@ -1,6 +1,6 @@
 const simpleGit = require('simple-git');
 const git = simpleGit();
-const Asena = require('../events');
+const Aqua = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 const Config = require('../config');
 const exec = require('child_process').exec;
@@ -12,7 +12,7 @@ const Language = require('../language');
 const Lang = Language.getString('updater');
 
 
-Asena.addCommand({pattern: 'update$', fromMe: true, desc: Lang.UPDATER_DESC}, (async (message, match) => {
+Aqua.addCommand({pattern: 'update$', fromMe: true, desc: Lang.UPDATER_DESC}, (async (message, match) => {
     await git.fetch();
     var commits = await git.log([Config.BRANCH + '..origin/' + Config.BRANCH]);
     if (commits.total === 0) {
