2da25b3e550ce757ed3d697825aa167e91dce8fb
iamMagma <98006497+iamMagma@users.noreply.github.com>
2022-03-05 20:29:54 +0530

    Update setlogo.js

--- plugins/setlogo.js
+++ plugins/setlogo.js
@@ -3,6 +3,7 @@ const {MessageType,Mimetype} = require('@adiwajshing/baileys');
 const config = require('../config');
 const axios = require('axios');
 const uploadFile = require('../lib/uploadfile')
+const {uploadByUrl} = require('cobrabot-patch')
 const Heroku = require('heroku-client');
 const heroku = new Heroku({
     token: config.HEROKU.API_KEY
@@ -15,6 +16,7 @@ var in_prog = ''
 var succ_set = ''
 var need_img = ''
 var need_stic = ''
+var need_logo = ''
 if (config.LANG == 'SI') {
         apikey_error = '*‼️ඔබ IMGBB APIKEY සකසා නොමැත.*'
         no_apikey = '*‼️ඔබ ඇතුළත් කර ඇති IMGBB APIKEY වලංගු නොවේ.*'
@@ -22,6 +24,7 @@ if (config.LANG == 'SI') {
         succ_set = '*✅සාර්ථකව සකසන ලදී.*'
        need_img = '*කරුණාකර පින්තූරයකට පිළිතුරු දෙන්න*'
         need_stic = '*කරුණාකර ස්ටිකරයකට පිළිතුරු දෙන්න*'
+     need_logo = '*කරුණාකර වීඩියෝවකට හෝ පින්තූරයකට රිප්ලයි කරන්න.*'
     }
     if (config.LANG == 'EN') {
        
@@ -31,6 +34,7 @@ if (config.LANG == 'SI') {
         succ_set = '*✅Succesfully setted* '
         need_img = '*Please Reply to an image*'
         need_stic = '*Please Reply to a sticker*'
+       need_logo = '*Please reply to a video or image.*'
     }
 
 Aqua.addCommand({ pattern: 'setalive ?(.*)', fromMe: true, dontAddCommandList: true, deleteCommand: false }, async (message, match) => {
@@ -92,3 +96,84 @@ Aqua.addCommand({ pattern: 'setstic ?(.*)', fromMe: true, dontAddCommandList: tr
     }
   
        })})
+
+Aqua.addCommand({ pattern: 'setwelcome ?(.*)', fromMe: true, dontAddCommandList: true, deleteCommand: false }, async (message, match) => {
+  if (message.reply_message === false) return await message.sendMessage(message.jid,need_logo, MessageType.text,  {  quoted: message.data });
+   var up =  await message.client.sendMessage(message.jid, in_prog , MessageType.text,  {  quoted: message.data })
+    const savedFilename = await message.client.downloadAndSaveMediaMessage({
+            key: {
+                remoteJid: message.reply_message.jid,
+                id: message.reply_message.id
+            },
+            message: message.reply_message.data.quotedMessage
+        });
+       await uploadFile(savedFilename).then( async (data) => { 
+    const result = data.result.url
+    if (data.result.mimetype.includes('image')){
+       const ibb =  await axios.get(`https://api.imgbb.com/1/upload?key=${config.IMGBB}&image=${result}`)
+     if (!ibb.data.data)return await message.client.sendMessage(message.jid, no_apikey,MessageType.text, {quoted: message.data});
+     if (ibb.statusText == 'OK'){                
+        await heroku.patch(baseURI + '/config-vars', { 
+                    body: { 
+                        ['LOGO_WELCOME']: ibb.data.data.url
+                    } 
+                });
+                await message.client.sendMessage(message.jid,succ_set , MessageType.text, {quoted: message.data})
+      }
+        
+    }else if(data.result.mimetype.includes('video')){
+        
+       const tele =  uploadByUrl(result)
+       await heroku.patch(baseURI + '/config-vars', { 
+                    body: { 
+                        ['LOGO_WELCOME']: tele.link
+                    } 
+                });
+                await message.client.sendMessage(message.jid,succ_set , MessageType.text, {quoted: message.data})
+ 
+    }else {
+        return await message.sendMessage(message.jid,need_logo, MessageType.text,  {  quoted: message.data });
+    }
+  
+       })})
+
+Aqua.addCommand({ pattern: 'setbye ?(.*)', fromMe: true, dontAddCommandList: true, deleteCommand: false }, async (message, match) => {
+  if (message.reply_message === false) return await message.sendMessage(message.jid,need_logo, MessageType.text,  {  quoted: message.data });
+   var up =  await message.client.sendMessage(message.jid, in_prog , MessageType.text,  {  quoted: message.data })
+    const savedFilename = await message.client.downloadAndSaveMediaMessage({
+            key: {
+                remoteJid: message.reply_message.jid,
+                id: message.reply_message.id
+            },
+            message: message.reply_message.data.quotedMessage
+        });
+       await uploadFile(savedFilename).then( async (data) => { 
+    const result = data.result.url
+    if (data.result.mimetype.includes('image')){
+       const ibb =  await axios.get(`https://api.imgbb.com/1/upload?key=${config.IMGBB}&image=${result}`)
+     if (!ibb.data.data)return await message.client.sendMessage(message.jid, no_apikey,MessageType.text, {quoted: message.data});
+     if (ibb.statusText == 'OK'){                
+        await heroku.patch(baseURI + '/config-vars', { 
+                    body: { 
+                        ['LOGO_BYE']: ibb.data.data.url
+                    } 
+                });
+                await message.client.sendMessage(message.jid,succ_set , MessageType.text, {quoted: message.data})
+      }
+        
+    }else if(data.result.mimetype.includes('video')){
+        
+       const tele =  uploadByUrl(result)
+       await heroku.patch(baseURI + '/config-vars', { 
+                    body: { 
+                        ['LOGO_BYE']: tele.link
+                    } 
+                });
+                await message.client.sendMessage(message.jid,succ_set , MessageType.text, {quoted: message.data})
+ 
+    }else {
+        return await message.sendMessage(message.jid,need_logo, MessageType.text,  {  quoted: message.data });
+    }
+  
+       })})
+
